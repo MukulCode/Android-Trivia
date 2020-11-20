df466db2b12d4684e3cb414ed17daff1dcf9ba37
Mukul Kumar <mukul.2121@gmail.com>
2020-11-20 22:35:12 +0530

    share button not implement since I was facing some errors

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,11 +16,12 @@
 
 package com.example.android.navigation
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -41,6 +42,31 @@ class GameWonFragment : Fragment() {
         if (args != null) {
             Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",Toast.LENGTH_LONG).show()
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu,menu)
+    }
+
+//    private fun getShareIntent() : Intent {
+//        val args = GameWonFragmentArgs.fromBundle(arguments())
+//        return ShareCompat.IntentBuilder.from(activity)
+//                .setText(R.string.share_success_text.toString(),args.numCorrect,args.numQuestions)
+//                .setType("text/plain")
+//                .intent
+//    }
+
+//    private fun shareSuccess(){
+//        startActivity(getShareIntent())
+//    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId){
+//            R.id.share -> shareSuccess()
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
 }
